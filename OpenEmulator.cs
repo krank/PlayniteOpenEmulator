61ec6326b7caf4e87133c742a4667cc158fca7e7
Mikael Bergström <krank23@gmail.com>
2024-06-15 02:14:36 +0200

    First working prototype

--- OpenEmulator.cs
+++ OpenEmulator.cs
@@ -4,6 +4,8 @@ using Playnite.SDK.Models;
 using Playnite.SDK.Plugins;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,28 +32,29 @@ namespace OpenEmulator
 
     public override IEnumerable<GameMenuItem> GetGameMenuItems(GetGameMenuItemsArgs args)
     {
-      /* TODO:
-       - Make a hashset of the exes instead of the emulators, or in addition to
-       - Display "Launch Ryujinx [ryujinx.exe]", one per exe in emulator installation folder
-       - Make subcategories IF there are multiple exes for the emulator
-      */
+      string OpenString = ResourceProvider.GetString("LOCOpenEmulatorOpen");
 
       ICollection<Emulator> emulators = GetEmulatorsOfGames(args.Games);
 
       foreach (Emulator emulator in emulators)
       {
-        yield return new GameMenuItem
-        {
-          Description = "Open " + emulator.Name,
+        string[] exeFiles = Directory.GetFiles(emulator.InstallDir, "*.exe");
 
-          Action = (gameArgs) =>
+        foreach (string exeFile in exeFiles)
+        {
+          yield return new GameMenuItem
           {
-            logger.Info("Launch " + emulator.Name);
-            // emulator.BuiltInConfigId
-            // emulator.AllProfiles.First().
-            PlayniteApi.
-          }
-        };
+            MenuSection = exeFiles.Length > 1 ? $"{OpenString} {emulator.Name}" : "",
+
+            Description = exeFiles.Length > 1 ? Path.GetFileName(exeFile) :
+               $"{OpenString} {emulator.Name} [{Path.GetFileName(exeFile)}]",
+
+            Action = (gameArgs) =>
+            {
+              Process.Start(exeFile);
+            }
+          };
+        }
       }
     }
 
@@ -80,48 +83,6 @@ namespace OpenEmulator
       }
 
       return emulators;
-    }
-
-
-
-    public override void OnGameInstalled(OnGameInstalledEventArgs args)
-    {
-      // Add code to be executed when game is finished installing.
-    }
-
-    public override void OnGameStarted(OnGameStartedEventArgs args)
-    {
-      // Add code to be executed when game is started running.
-    }
-
-    public override void OnGameStarting(OnGameStartingEventArgs args)
-    {
-      // Add code to be executed when game is preparing to be started.
-    }
-
-    public override void OnGameStopped(OnGameStoppedEventArgs args)
-    {
-      // Add code to be executed when game is preparing to be started.
-    }
-
-    public override void OnGameUninstalled(OnGameUninstalledEventArgs args)
-    {
-      // Add code to be executed when game is uninstalled.
-    }
-
-    public override void OnApplicationStarted(OnApplicationStartedEventArgs args)
-    {
-      // Add code to be executed when Playnite is initialized.
-    }
-
-    public override void OnApplicationStopped(OnApplicationStoppedEventArgs args)
-    {
-      // Add code to be executed when Playnite is shutting down.
-    }
-
-    public override void OnLibraryUpdated(OnLibraryUpdatedEventArgs args)
-    {
-      // Add code to be executed when library is updated.
     }
 
     public override ISettings GetSettings(bool firstRunSettings)
